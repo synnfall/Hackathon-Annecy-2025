a939f404044bd71dcd35bf29040a3cf9ec0087a9
TevaPhilippe05 <philippe.teva.pro@gmail.com>
2025-02-13 14:26:07 +0100

    Ajout header

--- footer.php
+++ footer.php
@@ -1,1 +1,3 @@
-a
+<footer>
+    TODO
+</footer>
--- header.php
+++ header.php
@@ -1,1 +1,10 @@
-a
+<header>
+    <div>Logo</div>
+    <a href="" class="chatbt">Cliquez ici</a>
+    <button id="chatbot_bouton" href=""></button>
+    <h1>Ingenieur au feminin</h1>
+    <button class="header_bouton" href="/">Accueil</button>
+    <button class="header_bouton" href="event">Evenement</button>
+    <button class="header_bouton" href="partenaire">Partenaire</button>
+    <button class="header_bouton" href="a_venir">A venir</button>
+</header>

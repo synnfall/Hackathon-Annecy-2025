07a912d810c174d30fbd5855dacc3ec4fc3e39e2
TevaPhilippe05 <philippe.teva.pro@gmail.com>
2025-02-13 15:05:57 +0100

    update header

--- header.php
+++ header.php
@@ -1,6 +1,5 @@
 <header>
     <div>Logo</div>
-    <a href="" class="chatbt">Cliquez ici</a>
     <button id="chatbot_bouton" href=""></button>
     <h1>Ingenieur au feminin</h1>
     <button class="header_bouton" href="/">Accueil</button>

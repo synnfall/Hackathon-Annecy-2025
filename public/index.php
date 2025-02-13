e88d527a0977a716f6b157e13badda0aa2b72050
synnfall <synnfall@gmail.com>
2025-02-13 14:06:03 +0100

    rooting

--- public/index.php
+++ public/index.php
@@ -3,11 +3,19 @@
 $url = $_SERVER['REQUEST_URI'];
 $path = explode('?', $url)[0];
 
-echo $path;
-
-// if ($path === '/') {
-//     echo $path;
-// } else {
-//     http_response_code(404);
-//     echo '404 Not Found';
-// }
+if ($path === '/') {
+    include __DIR__ . '/../index.html';
+} 
+elseif ($path === '/a_venir') {
+    include __DIR__ . '/../a_venir.php';
+}
+elseif ($path === '/event') {
+    include __DIR__ . '/../event.php';
+}
+elseif ($path === '/partenaires' || $path === '/partenaire') {
+    include __DIR__ . '/../partenaire.php';
+}
+else {
+    http_response_code(404);
+    echo '404 Not Found';
+}

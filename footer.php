f282f17753a67a11ef6f4a77343a6ed67b69f714
Outoine15 <outoine15@gmail.com>
2025-02-13 15:31:09 +0100

    style 5

--- footer.php
+++ footer.php
@@ -1,10 +1,12 @@
 <footer>
     <h2>Contactez-nous</h2>
-        <label for="email">E-mail :</label>
+<div>
+    <label for="email">E-mail :</label>
         <input type="email" id="email" name="email" required>
 
         <label for="message">Message :</label>
         <textarea id="message" name="message" rows="5" required></textarea>
 
         <button type="submit">Envoyer</button>
+        </div>
 </footer>

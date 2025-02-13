e03d8877fffc8d5608fbe09ed02edef42231886d
TevaPhilippe05 <philippe.teva.pro@gmail.com>
2025-02-13 14:05:07 +0100

    Init structure

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport">
+    <title>Ingenieur Au Féminin</title>
+    <link rel="stylesheet" href="public/styles/pages.css">
+</head>
+<body>
+
+<?php require_once('header.php'); ?>
+
+<main>
+    <!-- Section de présentation -->
+    <section>
+        <div>
+            <img src="assets/Logo_IAF.png" alt="Logo" class="logo">
+            <h1>Ingénieur au Féminin</h1>
+        </div>
+    </section>
+
+    <!-- Section des textes -->
+    <section>
+        <h2>Pourquoi choisir l’ingénierie ?</h2>
+        <p>Les femmes ont toute leur place dans le domaine de l'ingénierie. Découvrez des témoignages inspirants, des parcours et des opportunités pour faire évoluer votre carrière.</p>
+    </section>
+</main>
+
+<?php require_once("footer.php"); ?>
+</body>
+</html>

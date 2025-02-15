7b55cea75e5d1c9b652ce1175388181e5f628078
synnfall <synnfall@gmail.com>
2025-02-15 19:12:35 +0100

    test hooks encore

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ $path = explode('?', $url)[0];
 
 if ($path === '/') {
     require __DIR__ . '/../pages/index.php';
-} elseif ($path === '/news' || $path === '/new' || $path === '/nouvelles') {
+} elseif ($path === '/news') {
     require __DIR__ . '/../pages/news.php';
 }
 elseif ($path === '/contact') {
